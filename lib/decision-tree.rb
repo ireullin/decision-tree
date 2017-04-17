a15e528bbf983bf62448b37afd2c1abea341d349
ireullin <cheinheng.lin@uitox.com>
2017-04-17 17:39:41 +0800

    Modified README.md

--- lib/decision-tree.rb
+++ lib/decision-tree.rb
@@ -27,59 +27,7 @@ module Enumerable
 end
 
 
-def read_sample
-    buff = []
-    File.open('sample.csv','r') do |f|
-        f.each do |line|
-            cells = line.chomp.split(',')
-            buff << { features: cells[1..-1], label: cells[0]}
-        end
-    end
-    return buff
-end
-
-
-def transpose(m)
-    t = Array.new(m[0].size){Array.new(m.size)}
-    m.size.times do |i|
-        m[0].size.times do |j|
-            t[j][i] = m[i][j]
-        end
-    end
-    return t
-end
-
-
-def simpler_sample2
-    return [
-       	{features: [0,0,0,0],label: 'n'},
-		{features: [0,0,0,1],label: 'n'},
-		{features: [1,0,0,0],label: 'y'},
-		{features: [2,1,0,0],label: 'y'},
-		{features: [2,2,1,0],label: 'y'},
-		{features: [2,2,1,1],label: 'n'},
-		{features: [1,2,1,1],label: 'y'},
-    ]
-end
-
-
-def simpler_sample
-    return [
-        {features: [1,'A'], label: 'true'},
-        {features: [0,'B'], label: 'false'},
-        {features: [1,'A'], label: 'true'},
-        {features: [0,'A'], label: 'false'},
-        {features: [1,'C'], label: 'true'},
-        {features: [0,'C'], label: 'true'},
-        {features: [1,'B'], label: 'false'},
-        {features: [0,'A'], label: 'true'},
-        {features: [1,'A'], label: 'true'},
-        {features: [1,'C'], label: 'true'}
-     ]
-end
-
-
-class TreeNode
+class DecisionTree
 	def initialize(entries, columns=nil, algorithm='c45', dimension=nil, parent_node=nil, threshold=nil, path=nil)
 		@parent_node = parent_node
 		@path = if path.nil?
@@ -184,43 +132,7 @@ class TreeNode
 		end
 
 		buff.each do |feature_value,child_entries|
-			@child_nodes[feature_value] = TreeNode.new(child_entries, @columns, @algorithm, @dimension, self, feature_value, @path.dup)
+			@child_nodes[feature_value] = DecisionTree.new(child_entries, @columns, @algorithm, @dimension, self, feature_value, @path.dup)
 		end
 	end
 end
-
-
-
-def main
-
-    # data =read_sample
-    # puts data[0].to_json
-
-
-    data = simpler_sample2
-    root = TreeNode.new(data, ['outlook','temp','hum','windy'])
-    root.to_pseudo_code.each do |line|
-        puts line
-    end
-   	# i = root.choose_best_feature
-   	# puts root.split_by_feature(i).to_json
-
-
-
-    # label = data.map{|x| x[:label]}
-    # parent_entropy = label.entropy
-    # puts parent_entropy
-
-    # id3_ig1 = parent_entropy - data.map{|x| x[:features][0]}.entropy_with(label)
-    # c45_ig1 = id3_ig1 / data.map{|x| x[:features][0]}.entropy
-
-    # id3_ig2 = parent_entropy - data.map{|x| x[:features][1]}.entropy_with(label)
-    # c45_ig2 = id3_ig2 / data.map{|x| x[:features][1]}.entropy
-    # puts id3_ig1,id3_ig2
-    # puts c45_ig1,c45_ig2
-
-
-end
-
-main if __FILE__==$0
-
